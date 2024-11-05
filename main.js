3c632871d25a3959d503bad47604a38490855c34
Andromeda <aeondromach@gmail.com>
2024-11-04 22:50:33 -0700

    Huge Update
    - Table of Contents
    - Social Links

--- main.js
+++ main.js
@@ -1,12 +1,45 @@
 $(document).ready(function() {
-    document.getElementById("header").innerHTML = fetch("header.html").then(res => res.text());
-    document.getElementById("footer").innerHTML = fetch("footer.html").then(res => res.text());
-    navbuttons("#home","/home");
-    navbuttons('#feedback',"Feedback.php");
-    navbuttons('#campaigns',"Campaigns.php");
-    navbuttons('#dmOnly',"DmOnly.php");
-    navbuttons('#contact',"/contact");
-})
+    navbuttons(".about","/about");
+    navbuttons('.project',"/project");
+    navbuttons('.skills',"/skills");
+    navbuttons('.blog',"/blogs");
+    navbuttons('.resume',"/resume");
+    navbuttons('.contact',"/contact");
+    navbuttons('.git','https://github.com/aeondromach');
+    navbuttons('.linked','/error')
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const list = document.getElementById("leftList");
+    const headings = document.querySelectorAll("h2, h3, h4, h5");
+
+    headings.forEach(heading => {
+        const id = heading.textContent.replace(/\s+/g, '-').toLowerCase();
+        heading.id = id; // Set the ID for the heading
+
+        const li = document.createElement("li");
+        switch (heading.tagName) {
+            case 'H2':
+                li.innerHTML = `<a class="tableOfContents leftListh2" href="#${id}">${heading.textContent}</a>`;
+                li.className = "leftListh2";
+                break;
+            case 'H3':
+                li.innerHTML = `<a class="tableOfContents leftListh3" href="#${id}">${heading.textContent}</a>`;
+                li.className = "leftListh3";
+                break;
+            case 'H4':
+                li.innerHTML = `<a class="tableOfContents leftListh4" href="#${id}">${heading.textContent}</a>`;
+                li.className = "leftListh4"
+                break;
+            case 'H5':
+                li.innerHTML = `<a class="tableOfContents leftListh5" href="#${id}">${heading.textContent}</a>`;
+                li.className = "leftListh5"
+                break;
+        }
+
+        list.appendChild(li);
+    });
+});
 
 function navbuttons(take, give) {
     if ($(take).length > 0) {
